99003f5074b9fe251bd264f4c8c62d4a81d74b7f
Herciu Marian <herciumarian8@gmail.com>
2025-11-04 14:02:21 +0200

    Improved styles for HomePage

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Header from './Components/Header';
 import Main from './Components/Main';
@@ -6,13 +7,14 @@ import TechStack from './Components/TechStack';
 
 const HomePage = () => {
   return (
-    <div className='flex flex-col w-full h-screen justify-baseline'>
-        <Header />
-        <Main />
-        <TechStack />
-        <Projects />
+    <div className="flex flex-col w-full bg-gradient-to-b from-cyan-50 via-white to-gray-50">
+      <Header />
+      <Main />
+      <TechStack />
+      <Projects />
     </div>
   )
 }
 
 export default HomePage
+
